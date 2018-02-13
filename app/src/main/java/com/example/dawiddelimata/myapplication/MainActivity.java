7c78ec2ab7b555ce596a48ffaa930c781a04682d
Dawid Delimata <delimata.dawid@gmail.com>
2018-02-13 01:41:21 +0000

    Created tool bar menu and background

--- app/src/main/java/com/example/dawiddelimata/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dawiddelimata/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dawiddelimata.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        // Displaying created bar in xml
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("    BMI Calculator");                                           // Set name of new toolbar
+        getSupportActionBar().setLogo(R.drawable.scale);
     }
 }
